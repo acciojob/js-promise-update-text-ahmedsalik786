18e4aa5787f8a714719cc0bf56499a7a319c24b5
SALIK AHMED <92015547+ahmedsalik786@users.noreply.github.com>
2023-05-30 18:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
-function test() {
-	let prom=new Promise((resolve)=>{
-		setTimeOut(()=>{
-			resolve("Hello world!");
-		},1000)
-	});
-	prom.then((data)=>{
-		let text=document.createElement("div");
-		text.innerText=data;
-		document.body.append(text);
-	})
-}
+  function test() {
+            let prom=new Promise((resolve)=>{
+                setTimeout(()=>{
+                    resolve("Hello world!")
+                },1000)
+            });
+            prom.then((data)=>{
+                let text=document.createElement("div");
+                text.innerText=data;
+                document.body.append(text);
+            })
+        }
